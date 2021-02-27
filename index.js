6d7758f4b4fa84557612dcfebdfa98181981105c
Nagendra Reddy <nagendramca2011@gmail.com>
2021-02-27 19:38:54 +0530

    course API Added

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 const personalData = require("./database/person/person.json");
+const courseDB = require("./database/course/db-data.json");
+
 let port = process.env.PORT  || 1337;
 
 app.get("/", (req, res)=>{
     res.send("Hello World");
 });
 
+/*
+* Personal data API
+*/
 app.get("/api/person", (req, res)=>{   
-    res.send( JSON.stringify(personalData));
+    res.send(JSON.stringify(personalData));
+});
+/*
+* Course data API
+*/
+app.get("/api/course", (req, res)=>{   
+    res.send(JSON.stringify(courseDB));
 });
 
 app.listen(port, ()=>{
