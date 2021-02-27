106f473bcacb364f4ff9ffba411ef97dc9e44fce
Nagendra Reddy <nagendramca2011@gmail.com>
2021-02-27 18:39:05 +0530

    Initial commit with test api

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const personalData = require("./database/person/person.json");
+let port = process.env.port || 3000;
+
+app.get("/", (req, res)=>{
+    res.send("Hello World");
+});
+
+app.get("/api/person", (req, res)=>{   
+    res.send( JSON.stringify(personalData));
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on port http://localhost:${port}`);
+});
